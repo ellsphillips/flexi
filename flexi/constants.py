11012e188607d9ea7464d2ede8d4ed773ad57281
ellsphillips <elliott.phillips.dev@gmail.com>
2023-10-13 17:36:00 +0100

    Feature: clock action enum

---
+++ flexi/constants.py
@@ -0,0 +1,28 @@
+from enum import Enum
+
+
+class Clock(str, Enum):
+    """Actions for clocking in or out."""
+
+    IN = "in"
+    OUT = "out"
+
+    @classmethod
+    def from_str(cls, action: str) -> "Clock":
+        """Convert a clock action to its equivalent Enum.
+
+        Args:
+            action: The clock action, i.e. IN or OUT.
+
+        Returns:
+            The equivalent of the clock action in Enum.
+
+        Examples:
+            >>> Clock.from_str("out") is Clock.OUT
+            True
+            >>> Clock.from_str("in") is Clock.IN
+            True
+            >>> Clock.from_str("o") is Clock.OUT
+            True
+        """
+        return cls.OUT if action.lower().startswith("o") else cls.IN
