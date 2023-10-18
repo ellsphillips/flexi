95b706fa6661bc3628336e91f7bf7e28bbbe2660
ellsphillips <elliott.phillips.dev@gmail.com>
2023-10-18 20:00:29 +0100

    Fix: remove aux doctest

--- flexi/ui.py
+++ flexi/ui.py
@@ -4,17 +4,7 @@ import flexi
 
 
 def print_welcome() -> None:
-    """Prints the flexi welcome header.
-
-    Args:
-        None
-
-    Examples:
-        >>> print_welcome()
-         flexi  v0.0.1
-        Manage your working hours, flexibly.
-        <BLANKLINE>
-    """
+    """Print the flexi welcome title."""
     badge = click.style(" flexi ", fg="white", bg="cyan")
     strapline = click.style(flexi.__doc__, fg="bright_black")
     click.secho(f"{badge} v{flexi.__version__}\n{strapline}\n", bold=True)
