88dfbe7e8239f5f5d02cce7b74aac4e0c210580a
ellsphillips <elliott.phillips.dev@gmail.com>
2023-10-25 19:57:17 +0100

    Feature: session duration computed property

--- flexi/log/model.py
+++ flexi/log/model.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import datetime as dt
 from typing import TYPE_CHECKING, Optional
 
 import pydantic
@@ -22,6 +23,15 @@ class Session(pydantic.BaseModel):
 
     clock_in: str
     clock_out: str
+
+    @pydantic.computed_field  # type: ignore # noqa: PGH003
+    @property
+    def duration(self) -> dt.timedelta:
+        """Duration of the session."""
+        fmt = "%H:%M"
+        clock_out = dt.datetime.strptime(self.clock_out, fmt).astimezone()
+        clock_in = dt.datetime.strptime(self.clock_in, fmt).astimezone()
+        return clock_out - clock_in
 
 
 class Correction(pydantic.BaseModel):
