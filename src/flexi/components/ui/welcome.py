c5cd42b30347bda7d04be07a97cc563513f4ed91
ellsphillips <elliott.phillips.dev@gmail.com>
2025-01-11 19:01:19 +0000

    feat: initialise project

---
+++ src/flexi/components/ui/welcome.py
@@ -0,0 +1,11 @@
+import click
+
+import flexi
+
+
+def print_welcome() -> None:
+    """Print the flexi welcome title."""
+    badge = click.style(" flexi ", fg="white", bg="cyan")
+    strapline = click.style(flexi.__doc__, fg="bright_black")
+    click.clear()
+    click.secho(f"{badge} v{flexi.__version__}\n{strapline}\n", bold=True)
